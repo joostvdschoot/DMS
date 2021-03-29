4f07ecf1a41bc72ab298057abcc6757c2f6522cf
Joost van der Schoot <joostvdschoot@gmail.com>
2021-03-29 02:10:03 +0200

    IAM

--- grails-app/init/dms/BootStrap.groovy
+++ grails-app/init/dms/BootStrap.groovy
@@ -3,25 +3,33 @@ package dms
 import dms.Role
 import dms.User
 import dms.UserRole
+import dms.Book
 
 class BootStrap {
 
-  String usr = "me"
+  String usr = "admin"
   String pw = "password"
   String radm = "ROLE_ADMIN"
   String rusr = "ROLE_USER"
 
     def init = { servletContext ->
 
-      def b1 = new Book(title:"The Stand").save()
-      def b2 = new Book(title:"The Shining").save()
 
-      def testUser = new User(username:usr, password:pw, fullname:"Me", enabled:true, accountExpired:false,accountLocked:false,passwordExpired:false ).save()
+//      def b1 = new Book(title:'The Stand').save(flush:true,failOnError: true)
+//      def b2 = new Book(title:'The Shining').save(flush:true,failOnError: true)
+//      def b3 = new Book(title:'The Bible').save(flush:true,failOnError: true)
 
-      def adminRole = new Role(authority:radm).save()
-      def userRole = new Role(authority:rusr).save()
 
-      UserRole.create testUser, adminRole, true
+      def admUser = new User(username:"admin", password:"password", fullname:"Admin", enabled:true, accountExpired:false,accountLocked:false,passwordExpired:false ).save(flush:true)
+
+      def endUser = new User(username:"user", password:"secret", fullname:"User", enabled:true, accountExpired:false,accountLocked:false,passwordExpired:false ).save(flush:true)
+
+      def adminRole = new Role(authority:radm).save(flush:true,)
+      def userRole = new Role(authority:rusr).save(flush:true,)
+//      def testRole = new Role(authority:"ROLE_TEST").save(flush:true,)
+
+      UserRole.create admUser, adminRole, true
+      UserRole.create endUser, userRole, true
 
       //assert User.count() == 1
       //assert Role.count() == 2
